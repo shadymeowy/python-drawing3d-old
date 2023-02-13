816851b0b02fd37e0aec8f4fa4c58c1820df27fd
shadymeowy <tolgademirdal@gmail.com>
2023-02-13 18:19:18 +0300

    rename project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Drawing3D
+# python-drawing3D
 Easy 3D drawing library elevating 2D drawing libraries for visualization purposes
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='drawing3d',
+    name='python-drawing3d',
     version='0.0.1',
     description='Easy 3D drawing library elevating 2D drawing libraries for visualization purposes',
     author='Tolga Demirdal',
-    url='https://github.com/shadymeowy/drawing3d',
+    url='https://github.com/shadymeowy/python-drawing3d',
     setup_requires=[],
     install_requires=['numpy', 'scipy', 'PySide6'],
     packages=['drawing3d', 'drawing3d.draw', 'drawing3d.camera'],
