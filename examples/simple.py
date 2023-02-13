834ee480f52c99ccf2c292b388d0fb466b53ee68
shadymeowy <tolgademirdal@gmail.com>
2023-02-13 18:16:18 +0300

    a simple example

---
+++ examples/simple.py
@@ -0,0 +1,13 @@
+from drawing3d import draw3d
+
+draw = draw3d(renderer='pyside6')
+
+while True:
+    draw.begin()
+    draw.color = (255, 0, 0)
+    draw.style((255, 0, 0), 1.0, 1)
+    draw.line((0, 0, 0), (1, 1, 0))
+    draw.circle((0, 0, 0), 1)
+    draw.style((0, 255, 0), 1.0, 1)
+    draw.cube((0, 0, 0), (0, 0, 1))
+    draw.end(1 / 60)
